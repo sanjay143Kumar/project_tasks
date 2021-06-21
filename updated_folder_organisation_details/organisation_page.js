0e348328c3e0cd668fe24696ce1569b327edc481
sanjay143Kumar <83287911+sanjay143Kumar@users.noreply.github.com>
2021-06-22 00:21:13 +0530

    Add files via upload

---
+++ updated_folder_organisation_details/organisation_page.js
@@ -0,0 +1,40 @@
+function myWebsiteUrl() {
+    var x = document.getElementById('hide_for_url');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function mySocialMediaAccessUrl() {
+    var s = document.getElementById('hide_for_socialMedia');
+    if (s.style.display === 'none') {
+        s.style.display = 'block';
+    } else {
+        s.style.display = 'none';
+    }
+}
+function instagram_url() {
+    var u = document.getElementById('hide_for_socialMedia_url');
+    if (u.style.display === 'none') {
+        u.style.display = 'block';
+    } else {
+        u.style.display = 'none';
+    }
+}
+function linkedin_url() {
+    var linked = document.getElementById('hide_for_socialMedia_url2');
+    if (linked.style.display === 'none') {
+        linked.style.display = 'block';
+    } else {
+        linked.style.display = 'none';
+    }
+}
+function uploaddocument() {
+    var upload = document.getElementById('hide_for_file_upload');
+    if (upload.style.display === 'none') {
+        upload.style.display = 'block';
+    } else {
+        upload.style.display = 'none';
+    }
+}
